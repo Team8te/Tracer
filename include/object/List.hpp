c252014f1e68e377be96c8f86ac6eb9ea6c6c612
Alexander Sorokin <sorokinalexander@inbox.ru>
2019-10-17 00:40:26 +0300

    Implement TraceList iterator

--- include/object/List.hpp
+++ include/object/List.hpp
@@ -1,43 +1,88 @@
 #pragma once
 
 #include <stdexcept>
+#include <memory>
 
 namespace lazyant
 {
 namespace object
 {
 
+struct NodeList
+{
+    NodeList() = default;
+
+    virtual ~NodeList()
+    {
+        priv->next = std::move(next);
+        next->priv = std::move(priv);
+    }
+
+    std::unique_ptr<NodeList> next;
+    std::unique_ptr<NodeList> priv;
+};
+
+template<class T>
+struct Node
+{
+    T value;
+    std::unique_ptr<Node<T>> next;
+    std::unique_ptr<Node<T>> priv;
+};
+
+template<class T>
+class ListIteratorBase
+{
+public:
+    ListIteratorBase() = default;
+
+    ListIteratorBase(std::uniquer<Node<T>> node_ptr)
+        : _node_ptr(std::move(node_ptr))
+    {}
+
+    void insrement()
+    {
+        _node_ptr->next;
+    }
+
+private:
+    std::unique_ptr<Node<T>> _node_ptr;
+};
+
 template <class T>
 class TracedList
 {
-protected:
-    struct Element
-    {
-        T _value;
-        Element* next;
-    };
-
 public:
     TracedList() = default;
 
     void push_back(const T& value)
     {
-        std::runtime_error("Not implemented");
+        insert()
+    }
+
+    Iterator<T> insert(Position position, const T& value)
+    {
+        
     }
 
     bool empty() const
     {
-        throw std::runtime_error("Not implemented");
+        return _size != 0;
     }
 
     size_t size() const
     {
-        throw std::runtime_error("Not implemented");
+        return _size;
+    }
+
+    Iterator<T> begin() const
+    {
+
     }
 
 private:
-    Element* head;
-    Element* tail;
+    Iterator<T> begin;
+    Iterator<T> end;
 };
 
 }
