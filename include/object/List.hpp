91b097e344602e01c5f374ba2eb9a8072755a92d
Alexander Sorokin <sorokinalexander@inbox.ru>
2019-10-31 21:41:58 +0300

    Implement insert method

--- include/object/List.hpp
+++ include/object/List.hpp
@@ -12,22 +12,32 @@ struct NodeList
 {
     NodeList() = default;
 
+    NodeList(const std::shared_ptr<NodeList>& otherNext,
+             const std::shared_ptr<NodeList>& otherPrev)
+        : next(otherNext)
+        , priv(otherPrev)
+    {}
+
     virtual ~NodeList()
     {
         priv->next = std::move(next);
         next->priv = std::move(priv);
     }
 
-    std::unique_ptr<NodeList> next;
-    std::unique_ptr<NodeList> priv;
+    std::shared_ptr<NodeList> next;
+    std::shared_ptr<NodeList> priv;
 };
 
 template<class T>
-struct Node
+struct Node : public NodeList
 {
-    T value;
-    std::unique_ptr<Node<T>> next;
-    std::unique_ptr<Node<T>> priv;
+    T nodeValue;
+
+    Node(const T& value,
+         std::shared_ptr<NodeList> node_ptr)
+         : NodeList(node_ptr->next, node_ptr->priv)
+         , nodeValue(value)
+    {}
 };
 
 template<class T>
@@ -36,33 +46,87 @@ class ListIteratorBase
 public:
     ListIteratorBase() = default;
 
-    ListIteratorBase(std::uniquer<Node<T>> node_ptr)
-        : _node_ptr(std::move(node_ptr))
+    ListIteratorBase(const std::shared_ptr<NodeList>& node_ptr)
+        : _node_ptr(node_ptr)
     {}
 
-    void insrement()
+    void increment()
     {
-        _node_ptr->next;
+        _node_ptr = _node_ptr->next;
     }
 
-private:
-    std::unique_ptr<Node<T>> _node_ptr;
+    void decrement()
+    {
+        _node_ptr = _node_ptr->priv;
+    }
+
+    T& operator*()
+    {
+        return std::static_pointer_cast<Node<T>>(_node_ptr)->nodeValue;
+    }
+
+protected:
+    std::shared_ptr<NodeList> _node_ptr;
 };
 
 template <class T>
 class TracedList
 {
 public:
+    class Iterator : public ListIteratorBase<T>
+    {
+    friend class TracedList;
+
+    public:
+        Iterator() = default;
+
+        Iterator(const std::shared_ptr<Node<T>>& node_ptr)
+            : ListIteratorBase<T>(node_ptr)
+        {}
+
+        ~Iterator() = default;
+
+        Iterator next()
+        {
+            return Iterator(this->_node_ptr->next);
+        }
+
+        Iterator &operator++()
+        {
+            this->increment();
+            return *this;
+        }
+
+        Iterator &operator--()
+        {
+            this->decrement();
+            return *this;
+        }
+    };
+
+    using Pointer = T*;
+    using Reference = T&;
+    using ConstIterator = const Iterator;
+
     TracedList() = default;
+
+    void push_from(const T& value)
+    {
+        return insert(begin(), value);
+    }
 
     void push_back(const T& value)
     {
-        insert()
+        insert(end(), value);
     }
 
-    Iterator<T> insert(Position position, const T& value)
+    Iterator insert(ConstIterator& position, const T& value)
     {
-        
+        auto element = std::make_shared<Node<T>>(value, position._node_ptr);
+        Iterator itr(std::move(element));
+        ++_size;
+
+        return itr;
     }
 
     bool empty() const
@@ -75,14 +139,19 @@ public:
         return _size;
     }
 
-    Iterator<T> begin() const
+    Iterator begin() const
     {
+        return _node.next();
+    }
 
+    Iterator end() const
+    {
+        return _node;
     }
 
 private:
-    Iterator<T> begin;
-    Iterator<T> end;
+    Iterator _node;
+    size_t _size;
 };
 
 }
